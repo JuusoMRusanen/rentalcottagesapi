3804236a5e70e2b323623b5953e465d6939c392a
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-08 23:04:11 +0200

    ability to save new cottages

--- controllers/cottage.controller.js
+++ controllers/cottage.controller.js
@@ -1,56 +1,144 @@
 //const db = require("../models/index.dev"); // DEVELOPMENT PATH
 const db = require("../models/index"); // PRODUCTION PATH
-
 const { QueryTypes } = require('sequelize');
+const multer = require("multer");
 
 const Cottage = db.cottage;
+const Photo = db.photo;
 const Op = db.Sequelize.Op;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, '/home/juurus/Github/rentalcottagesapi/public/photos/')
+  },
+  filename: function (req, file, cb) {
+
+    cb(null, "cottagephoto" + '-' + Date.now() + ".jpg")
+  }
+})
+
+const upload = multer({ storage: storage }).array('files', 6)
 
 // Create and Save a new Cottage
 exports.create = async (req, res) => {
-  // Validate request
-  if (!req.body.name) {
-    res.status(400).send({
-      message: "Content can not be empty!"
+
+  async function getLastIDs() {
+
+    let lastCottageId = 1;
+    let lastPhotoId = 1;
+    
+    // Get the last cottage in database
+    await Cottage.sequelize.query(
+      'SELECT '+
+      'cottage.id AS "cottageId" '+
+      'FROM cottage '+
+      'ORDER BY "cottageId" DESC '+
+      'LIMIT 1',
+      { type: QueryTypes.SELECT })
+    .then(data => {
+      lastCottageId = data[0].cottageId;
     });
-    return;
+
+    // Get the last photo in database
+    await Photo.sequelize.query(
+      'SELECT '+
+      'photo.id AS "photoId" '+
+      'FROM photo '+
+      'ORDER BY "photoId" DESC '+
+      'LIMIT 1',
+      { type: QueryTypes.SELECT })
+    .then(data => {
+      lastPhotoId = data[0].photoId;
+    });
+
+    return({
+      lastCottageId : lastCottageId, 
+      lastPhotoId : lastPhotoId
+    });
   }
 
-  let lastCottageId = 1;
-  
-  await Cottage.sequelize.query(
-    'SELECT '+
-    'cottage.id AS "cottageId" '+
-    'FROM cottage '+
-    'ORDER BY "cottageId" DESC '+
-    'LIMIT 1',
-    { type: QueryTypes.SELECT })
-  .then(data => {
-    lastCottageId = data[0].cottageId;
-  });
+  upload(req, res, function (err) {
+    
+    if (err instanceof multer.MulterError) {
+      console.log(err);
+      
+      // A Multer error occurred when uploading.
+    } 
+    
+    else if (err) {
+      console.log(err);
+      
+      // An unknown error occurred when uploading.
+    }
 
-  // Create a Cottage
-  const cottage = {
-    id: lastCottageId + 1,
-    cityId: req.body.cityId,
-    name: req.body.name,
-    bedrooms: req.body.bedrooms,
-    bathrooms: req.body.bathrooms,
-    price: req.body.price,
-    size: req.body.size,
-  };
+    // Everything went fine.
+    if (req.files != undefined) {
+      if (req.files.length > 0) {
 
-  // Save Cottage in the database
-  await Cottage.create(cottage)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Cottage."
+        let files = [];
+        
+        req.files.forEach(file => {
+          files.push(file);
+        })
+        
+        savePhotos(files);
+      }
+    } else {
+      console.log("Files are undefined.")
+    }
+    
+  })
+
+  // Save Photos in the database
+  async function savePhotos (files) {
+
+    // Create Photos and save them in the database
+    await getLastIDs().then(({ lastCottageId, lastPhotoId }) => {
+
+      // Create a Cottage
+      const cottage = {
+        id: lastCottageId + 1,
+        cityId: req.body.cityId,
+        name: req.body.name,
+        bedrooms: req.body.bedrooms,
+        bathrooms: req.body.bathrooms,
+        price: req.body.price,
+        size: req.body.size,
+      };
+
+      // Save Cottage in the database
+      Cottage.create(cottage)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Cottage."
+        });
       });
-    });
+      
+      for (let idx = 0; idx < files.length; idx++) {
+
+        let file = files[idx]
+
+        const photo = {
+          id: lastPhotoId + (idx + 1),
+          cottageId: lastCottageId + 1,
+          src: `/photos/${file.filename}`,
+          priority: idx,
+        };
+
+        console.log(photo);
+
+        Photo.create(photo)
+        .catch(err => { 
+          console.log( "Error! : "+err ) 
+        })
+      }
+    })
+  }
+
 };
 
 // Retrieve all cottages from the database.
