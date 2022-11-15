accdbc112ebda56160d4c06bcc1fc4081221a59c
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-15 13:32:06 +0200

    create new sequelize object

--- models/index.js
+++ models/index.js
@@ -31,7 +31,7 @@ app.use(express.static(__dirname + '/../public'));
 let sequelize;
 if (config.use_env_variable) {
   // sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  process.env.DATABASE_URL, {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -39,7 +39,13 @@ if (config.use_env_variable) {
         rejectUnauthorized: false
       }
     }
-  }
+  });
+  sequelize.authenticate().then(() => {
+    console.log("Connection successful!")
+  }).catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+  
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
   sequelize.authenticate().then(() => {
