6a13e5b6d7541797e1118b642c14c09d7ecb1475
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-15 15:35:52 +0200

    configuring

--- models/index.js
+++ models/index.js
@@ -58,7 +58,7 @@ app.get("/", (req, res) => {
 let sequelize;
 if (config.use_env_variable) {
   console.log("Connecting in production enviroment...")
-  //sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  //sequelize = new Sequelize(process.env[config.use_env_variable], config); 
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
