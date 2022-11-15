40f40180ced39123b29072a0b63ee58a8be6add1
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2022-11-15 14:06:26 +0200

    configuring

--- models/index.js
+++ models/index.js
@@ -39,8 +39,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 }
 );
 
-sequelize
-  .authenticate()
+sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
   })
